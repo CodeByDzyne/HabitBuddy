35d86537949702f24abae28494916cbad701e37f
CodeByDzyne <wdenley@dzynesolutions.com>
2022-12-01 11:23:37 +1100

    Initial Commit

---
+++ HabitBuddy/HabitBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitBuddyApp.swift
+//  HabitBuddy
+//
+//  Created by Wealthy Waz on 1/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
